cd1124ffbb43cb48533d2a42762266696344a60f
felix-do-wizardry <felix.do.1030@gmail.com>
2022-02-17 12:41:36 +0000

    moved module code to custom file

--- deit/models.py
+++ deit/models.py
@@ -4,9 +4,11 @@ import torch
 import torch.nn as nn
 from functools import partial
 
-from timm.models.vision_transformer import VisionTransformer, _cfg
+# from timm.models.vision_transformer import VisionTransformer, _cfg
 from timm.models.registry import register_model
 from timm.models.layers import trunc_normal_
+
+from models.vision_transformer import VisionTransformer, _cfg
 
 
 __all__ = [
