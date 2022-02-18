77877083872b8661783f5c87bdfe67445fe46871
felix-do-wizardry <felix.do.1030@gmail.com>
2022-02-18 13:47:43 +0000

    revert metrics only changes

--- deit/engine.py
+++ deit/engine.py
@@ -29,7 +29,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: DistillationLoss,
     header = 'Epoch: [{}]'.format(epoch)
     print_freq = 10
 
-    _index = 0
+    # _index = 0
     for samples, targets in metric_logger.log_every(data_loader, print_freq, header):
         samples = samples.to(device, non_blocking=True)
         targets = targets.to(device, non_blocking=True)
@@ -62,9 +62,9 @@ def train_one_epoch(model: torch.nn.Module, criterion: DistillationLoss,
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
         
         
-        _index += 1
-        if batch_limit and _index >= batch_limit:
-            break
+        # _index += 1
+        # if batch_limit and _index >= batch_limit:
+        #     break
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
     print("Averaged stats:", metric_logger)
@@ -81,7 +81,7 @@ def evaluate(data_loader, model, device, batch_limit=None):
     # switch to evaluation mode
     model.eval()
     
-    _index = 0
+    # _index = 0
     for images, target in metric_logger.log_every(data_loader, 10, header):
         images = images.to(device, non_blocking=True)
         target = target.to(device, non_blocking=True)
@@ -98,9 +98,9 @@ def evaluate(data_loader, model, device, batch_limit=None):
         metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
         metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
         
-        _index += 1
-        if batch_limit and _index >= batch_limit:
-            break
+        # _index += 1
+        # if batch_limit and _index >= batch_limit:
+        #     break
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
     print('* Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f} loss {losses.global_avg:.3f}'
